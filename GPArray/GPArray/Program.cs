204e5df201c5d8847387cc0d671c514b0e838fa6
galeNight <97892095+galeNight@users.noreply.github.com>
2022-08-17 13:21:35 +0200

    Add files via upload

--- GPArray/GPArray/Program.cs
+++ GPArray/GPArray/Program.cs
@@ -4,6 +4,7 @@
     {
         //the arrat for the detale of the argument int text
         static string[] arrangements = new string[100];
+        //multidimensional arrat:int[100,3]={0,1,2}{0,1,2}...x100)
         static int[,] tickets = new int[1000, 2];
         static void Main(string[] args)
         {
@@ -37,32 +38,50 @@
                     break;
             }
         }
+        /// <summary>
+        /// buyticket add to ticket array the number of ticket and the arengments
+        /// </summary>
+        /// <returns>int next free spot</returns>
         static int buytecket()
         {
             Console.Write("indtast nr på argument du ønsker at købe billet");
-            string indput = Console.ReadLine();
+            string? indput = Console.ReadLine();
+            // get the string and tries to convert to int 
+            //if succes returns is true and it output as int
+            //if fails nothing hapens
             int.TryParse(indput,out int arrangementsNumber);
             Console.Write("intast antalønsked billeter");
             indput = Console.ReadLine();
             int.TryParse(indput, out int amountoftickets);
-
             int freesport=Getnextfreespotinticketarray();
+            // we insert out tecket data in our multiD array
+            //first pos 0 then 1st pos be
             tickets[freesport, 0] = amountoftickets;
             tickets[freesport, 1] = arrangementsNumber;
             return freesport;
         }
+        /// <summary>
+        /// show all the ickets that have been purchased by looping throgh all 
+        /// of the multi array adn stopping when its emty
+        /// </summary>
         static void showticketbought()
         {
             Console.WriteLine("antal\tarrangement\tlokation");
             for (int i = 0; i < tickets.Length; i++)
             {
+                //if its emty we stop the loop
                 if (tickets[i, 0] == 0) return;
+                //otherwise we print it pn screen
                 string arr = arrangements[tickets[i, 1]];
                 string[] splitarray = arr.Split("-");
                 Console.WriteLine(tickets[i, 0] + "\t" + splitarray[0] + "\t" + splitarray[1]);
 
             }
         }
+        /// <summary>
+        /// loops through alle the ticket array and return first empty sport
+        /// </summary>
+        /// <returns></returns>
         static int Getnextfreespotinticketarray()
         {
             for (int i = 0; i < tickets.Length; i++)
