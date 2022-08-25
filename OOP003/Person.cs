db46660caa889ef88e81dd8b5a5058b333077317
galeNight <97892095+galeNight@users.noreply.github.com>
2022-08-25 08:18:30 +0200

    Add files via upload

---
+++ OOP003/Person.cs
@@ -0,0 +1,59 @@
+﻿namespace OOP003
+{
+    internal class Person
+    {
+        public string name;
+        public DateTime dob;
+        private string email;
+        public string password;
+        public int Age { get { return Getage(); } }
+        public int Getage()
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - dob.Year;
+            if (today < new DateTime(today.Year, dob.Month, dob.Day)) age--;
+            return age;
+        }
+
+        public string Parsword
+        {
+            get {return password; }
+            set
+            {
+                if (value.Length > 6 && value.Any(char.IsLower) && value.Any(char.IsDigit) && value.Any(char.IsUpper) && !value.Contains(" ")) ;
+                else Console.WriteLine("password skal misv være 6 sam have store ,små og tal med i uden space");
+                
+            }
+        }
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                if (value.Contains("@") && value.Contains("."))
+                {
+                    email = value;
+                }
+                else
+                {
+                    Console.WriteLine("der skal være @ og . i din email");
+                    email = null;
+                }
+            }
+        }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if(value==" ")
+                {
+                    Console.WriteLine("prøv igen");
+                    name = null;
+                }
+                else name = value;
+            }
+        }
+    }
+
+}
---
+++ OOP003/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace OOP003
+{
+    class Program
+    {
+        static void Main(string[]ages)
+        {
+            Person myperson = new Person();
+            Navn(myperson);
+            Console.WriteLine("hey "+ myperson.name);
+            Password(myperson);
+            Console.WriteLine("password er "+ myperson.password);
+            Age(myperson);
+            Console.WriteLine(" din alder er "+ myperson.Age);
+            Email(myperson);
+            Console.WriteLine("din email er "+ myperson.Email);
+
+        }
+        static void Navn(Person myperson)
+        {
+            do
+            {
+                Console.WriteLine("Indtast navn");
+                myperson.name = Console.ReadLine();
+            }
+            while (myperson.name==null);
+        }
+        static void Email(Person myperson)
+        {
+            do
+            {
+                Console.WriteLine("indtast email");
+                myperson.Email = Console.ReadLine();
+            }
+            while(myperson.Email == null);
+        }
+        static void Password(Person myperson)
+        {
+            do
+            {
+                Console.WriteLine("indtast password");
+                myperson.password = Console.ReadLine();
+            }
+            while(myperson.password == null);
+        }
+        static void Age(Person myperson)
+        {
+            myperson.dob=new DateTime(1995,12,26);
+            Console.WriteLine($"alder {myperson.Age}");
+        }
+      
+    }
+}
