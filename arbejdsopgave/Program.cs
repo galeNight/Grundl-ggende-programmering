f5775e77fddf0bce8f6e148f1490427c9007c9ec
galeNight <97892095+galeNight@users.noreply.github.com>
2022-09-05 13:15:49 +0200

    Add files via upload

---
+++ arbejdsopgave/Program.cs
@@ -0,0 +1,20 @@
+﻿using arbejdsopgave;
+
+skoletaske Ataske = new();
+madkasse Amadkasse=new madkasse();
+mad spejsepølse=new mad();
+Ataske.mad= Amadkasse;
+spejsepølse.navn="sigurd";
+Amadkasse.madliste.Add(spejsepølse);
+Ataske.penalhus=new penalhus();
+Ataske.penalhus.viskelæderliste = new();
+Ataske.penalhus.viskelæderliste.Add(new viskelæder() { navn = "jordbær" });
+
+foreach(viskelæder viskelæder in Ataske.penalhus.viskelæderliste)
+{
+    Console.WriteLine("viskelædder: " + viskelæder.navn);
+}
+foreach(mad mad in Ataske.mad.madliste)
+{
+    Console.WriteLine("mad: " + mad.navn);
+}
---
+++ arbejdsopgave/skole.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arbejdsopgave
+{
+    internal class skole
+    {
+        public string? navn { get; set; }
+        public int vægt { get; set; }
+        public int conditions { get; set; }
+    }
+    internal class skoletaske: skole
+    {
+        public penalhus? penalhus { get; set; }
+        public List<bøger>? bøgliste { get; set; }
+        public madkasse? mad { get; set; }
+    }
+    internal class bøger: skole
+    {
+        public string? forfatter { get; set; }
+    }
+    internal class penalhus : skole
+    {
+        public List<blyanter>?  blyanterliste { get; set; }
+        public List<viskelæder>? viskelæderliste { get; set; }
+    }
+    internal class viskelæder : skole
+    {
+        public string? lugt { get; set; }
+    }
+    internal class blyanter : skole
+    {
+        public string? farve { get; set; }
+    }
+    internal class madkasse : skole
+    {
+        public List<mad>? madliste { get; set; }=new();
+    }
+    internal class mad : skole
+    {
+        public string? smag { get; set; }
+    }
+}
